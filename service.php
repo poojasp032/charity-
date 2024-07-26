4d03d729061bfe4c5a98a4fb2240e39b1ce2c68e
poojasp032 <poojasp032@gmail.com>
2024-07-26 16:50:58 +0530

    Add files via upload

---
+++ service.php
@@ -0,0 +1,68 @@
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ASHTHA CHARITY TRUST</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="topnav">
+    <a href="./index.php">HOME</a>
+    <a href="./about.php">ABOUT US</a>
+    <a href="./service.php">SERVICES</a>
+    <a href="./donate.php">DONATE</a>
+    <a href="#" style="float:right">LOGIN</a>
+  </div>
+  
+<div class="hero"> <h1>ASHTHA CHARITABLE TRUST</h1>
+  <p><h6>DONATE FOR THOSE WHO ARE IN NEED</p>
+  <p>No one is left alone here,everyone is cared His Grace shines through ASHTHA</h6></p>
+  <button class ="button">DONATE</button>
+</div>
+
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5></h5>
+      <div class="img" style="height:400px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+    <div class="card">
+      <h2> </h2>
+      <h5> </h5>
+      <div class="img" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>About Me</h2>
+      <div class="img" style="height:100px;">Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    </div>
+    <div class="card">
+      <h3>Popular Post</h3>
+      <div class="img"><p>Image</p></div>
+      <div class="img"><p>Image</p></div>
+      <div class="img"><p>Image</p></div>
+    </div>
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+</html>
+
+
